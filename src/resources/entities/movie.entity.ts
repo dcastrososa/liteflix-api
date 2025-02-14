2cbd259fa09554bc1b9bfd5200fa64d3050fea66
Daniel <daniel@MacBook-Pro-3.local>
2025-02-13 20:28:07 -0400

    adding pk to movie table

--- src/resources/entities/movie.entity.ts
+++ src/resources/entities/movie.entity.ts
@@ -1,8 +1,8 @@
-import { Entity, Column, PrimaryColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Movie {
-    @PrimaryColumn()
+    @PrimaryGeneratedColumn()
     id: number;
 
     @Column({ unique: true })
